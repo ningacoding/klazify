255cb2a7abbbfbb5cf5230a2155d83c901785ff5
ningacoding <an_email>
2022-07-16 20:41:17 -0500

    v 1.1.2-beta7

--- modules/test.js
+++ modules/test.js
@@ -2,13 +2,10 @@ const _mergeWith = require('lodash.mergewith');
 const _merge = require('lodash.merge');
 
 const source = {
-  other: 'ok',
-  transforms: [{translate: 'translate'}],
+  transform: [{translate: '100%'}],
 };
 const target = {
-  test: 'fail',
-  other: 'fails',
-  transforms: [{rotate: 'something'}],
+  transform: [{rotate: 'something'}],
 };
 
 console.log('RESULT', _mergeWith(source, target, (objValue, srcValue) => {
